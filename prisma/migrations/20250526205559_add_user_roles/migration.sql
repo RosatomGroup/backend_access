0a9e76a58a3b2c457981f75acaaf1bdd8babce9f
Santa <glazinet1209@yandex.ru>
2025-05-29 21:05:16 +0300

    удалил ненужное

--- prisma/migrations/20250526205559_add_user_roles/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "AccessLevel" AS ENUM ('ADMIN', 'USER');
-
--- AlterTable
-ALTER TABLE "Request" ADD COLUMN     "access" "AccessLevel" NOT NULL DEFAULT 'USER';
--- prisma/migrations/20250527201357_rename_role_access/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `access` on the `User` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "User" DROP COLUMN "access",
-ADD COLUMN     "accessLevel" "AccessLevel" NOT NULL DEFAULT 'USER';
